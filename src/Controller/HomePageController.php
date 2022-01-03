5e1bb21ca5f67b6b0cf6516357741f44c259217d
kizeev <kizalvic@gmail.com>
2022-01-04 00:43:13 +0300

    create db

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -7,9 +7,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
-    #[Route("/home")]
+    #[Route("/", name: "home")]
     public function welcome()
     {
-        return $this->render("base.html.twig");
+        return $this->render("base.html.twig", [
+            "title" => "HomePage",
+        ]);
     }
 }
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', type: EmailType::class,options: array(
+                'label' => 'Enter Email',
+            ))
+            ->add('name', type: TextType::class, options: array(
+                'label' => 'Enter your name',
+            ))
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
